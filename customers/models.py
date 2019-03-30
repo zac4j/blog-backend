f4494bb82bda159717efde84f4c78be9bece3709
Zac <zac_ju@163.com>
2019-03-30 11:44:39 +0800

    create rest api

--- customers/models.py
+++ customers/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 
 class Customer(models.Model):
     first_name = models.CharField("First name", max_length=255)
@@ -10,6 +9,6 @@ class Customer(models.Model):
     address = models.TextField(blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     createdAt = models.DateTimeField("Created At", auto_now_add=True)
-    
+
     def __str__(self):
         return self.first_name
--- customers/views.py
+++ customers/views.py
@@ -1,3 +1,70 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 
-# Create your views here.
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .models import Customer
+from .serializers import *
+
+
+@api_view(['GET', 'POST'])
+def customers_list(request):
+    """
+    List customer, or create a new customer.
+    """
+    if request.method == 'GET':
+        data = []
+        next_page = 1
+        previous_page = 1
+        customers = Customer.objects.all()
+        page = request.GET.get('page', 1)
+        paginator = Paginator(customers, 10)
+        try:
+            data = paginator.page(page)
+        except PageNotAnInteger:
+            data = paginator.page(1)
+        except EmptyPage:
+            data = paginator.page(paginator.num_pages)
+
+        serializer = CustomerSerializer(data, context={'request': request}, many=True)
+        if data.has_next():
+            next_page = data.next_page_number()
+        if data.has_previous():
+            previous_page = data.previous_page_number()
+
+        return Response({'data': serializer.data, 'count': paginator.count, 'numpage': paginator.num_pages,
+                         'nextlink': '/api/customers/?page=' + str(next_page),
+                         'prevlink': '/api/customers/?page=' + str(previous_page)})
+
+    elif request.method == 'POST':
+        serializer = CustomerSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def customers_detail(request, pk):
+    """
+    Retrieve, update or delete a customer by id/pk
+    """
+    try:
+        customer = Customer.objects.get(pk=pk)
+    except Customer.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = CustomerSerializer(customer, context={'request': request})
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = CustomerSerializer(customer, data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        customer.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
